084a642918fe05c9f903a1219c35c73741053dcc
Suphakit P <me@suphakit.net>
2024-10-25 00:27:38 +0700

    feat<animation>: added animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -12,8 +12,17 @@ window.addEventListener("load", (event) => {
   }
 
   setTimeout(() => {
+    document.querySelector(".info-box > h1").classList.toggle("hide");
+  }, 2000);
+
+  setTimeout(() => {
+    document.querySelector(".info-box > p").classList.toggle("hide");
+  }, 2200);
+
+  setTimeout(() => {
     for (let i = 0; i < cover.length; i++) {
       cover[i].classList.toggle("no-delay");
+      document.querySelector(".info-box").classList.toggle("no-delay");
     }
   }, 5000);
 
