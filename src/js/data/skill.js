ea7352bf415718833219ebb89e12e2398fa157df
Suphakit P <me@suphakit.net>
2025-01-04 15:16:33 +0700

    feat<components>: added tech stats part

---
+++ src/js/data/skill.js
@@ -0,0 +1,172 @@
+const Skill = [
+  {
+    name: "HTML5",
+    link: "https://www.w3.org/html/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/html.png",
+  },
+  {
+    name: "CSS3",
+    link: "https://www.w3schools.com/css/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/css.png",
+  },
+  {
+    name: "JavaScript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/js.png",
+  },
+  {
+    name: "Bootstrap",
+    link: "https://getbootstrap.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/bs.png",
+  },
+  {
+    name: "Tailwind.css",
+    link: "https://tailwindcss.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/tw.png",
+  },
+  {
+    name: "Docker",
+    link: "https://www.docker.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/docker.png",
+  },
+  {
+    name: "ionic",
+    link: "https://ionicframwork.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/ionic.png",
+  },
+  {
+    name: "Cordova",
+    link: "https://cordova.apache.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/cordova.png",
+  },
+  {
+    name: "Jekyll",
+    link: "https://jekyllrb.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/jekyll.png",
+  },
+  {
+    name: "Electron",
+    link: "https://www.electronjs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/electron.png",
+  },
+  {
+    name: "express.js",
+    link: "https://expressjs.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/express.png",
+  },
+  {
+    name: "Svelte.js",
+    link: "https://svelte.dev/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/svelte.png",
+  },
+  {
+    name: "Canvas.js",
+    link: "https://canvasjs.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/canvas.png",
+  },
+  {
+    name: "Hexo",
+    link: "https://hexo.io/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/hexo.png",
+  },
+  {
+    name: "React.js",
+    link: "https://reactjs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/react.png",
+  },
+  {
+    name: "Vue.js",
+    link: "https://vuejs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/vue.js.png",
+  },
+  {
+    name: "Nuxt.js",
+    link: "https://nuxtjs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/nuxt.png",
+  },
+  {
+    name: "Pug",
+    link: "https://pugjs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/pug.png",
+  },
+  {
+    name: "Puppeteer",
+    link: "https://github.com/puppeteer/puppeteer",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/puppet.png",
+  },
+  {
+    name: "node.js",
+    link: "https://nodejs.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/node.png",
+  },
+  {
+    name: "Typescript",
+    link: "https://typescriptlan.org/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/ts.png",
+  },
+  {
+    name: "PHP",
+    link: "https://php.net/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/php.png",
+  },
+  {
+    name: "C Lang",
+    link: "https://www.cprogramming.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/c.png",
+  },
+  {
+    name: "C Sharp",
+    link: "https://www.w3schools.com/cs/index.php",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/cs.png",
+  },
+  {
+    name: "Mongo DB",
+    link: "https://mongodb.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/mongo.png",
+  },
+  {
+    name: "MySQL",
+    link: "https://mysql.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/mysql.png",
+  },
+  {
+    name: "Git",
+    link: "https://git-scm.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/git.png",
+  },
+  {
+    name: "Figma",
+    link: "https://figma.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/figma.png",
+  },
+  {
+    name: "IFTTT",
+    link: "https://ifttt.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/ifttt.png",
+  },
+  {
+    name: "Photoshop",
+    link: "https://photoshop.com/en",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/ps.png",
+  },
+  {
+    name: "Postman",
+    link: "https://postman.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/postman.png",
+  },
+  {
+    name: "Zapier",
+    link: "https://zapier.com/",
+    img: "https://cdn.jsdelivr.net/gh/dont-tattled-on-me/dont-icon@latest/src/zap.png",
+  },
+];
+
+const techElement = document.querySelector("#tech-stats-list");
+
+for (let i = 0; i < Skill.length; i++) {
+  const Template = `<a href="${Skill[i].link}" class="tech-icon" style="background: url(${Skill[i].img})">
+  
+</a>`;
+
+  techElement.innerHTML += Template;
+}
